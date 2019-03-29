f77b0919199740ed276e2ad9139669218e74c188
Anıl Aras <anilaras1@gmail.com>
2019-03-30 01:32:30 +0300

    Update not_shittiy_shiftOut(w\libgpio).c

--- not_shittiy_shiftOut(w\libgpio).c
+++ not_shittiy_shiftOut(w\libgpio).c
@@ -22,6 +22,26 @@ int latchPin = 127;        //CON3-P12 PH2  shift register latch pini
 int clockPin = 172;       //CON3-P11 PI19  shift register clock pini
 int dataPin = 171;	  //CON3-P13 PI18 shift register data pini
 
+void registerWrite(int value);
+void shiftOut(int dataPin, int clockPin, int bitOrder, int val);
+void sigintHandler(int sig_num);
+void settings();
+
+
+int main(int argc, char **argv){
+    settings();
+    signal(SIGINT, sigintHandler);
+    for (int c = 0; c < 256; c++) {
+        registerWrite(c);
+        usleep(50000);
+    }
+    usleep(50000);
+    for (int i = 0; i < 2; i++) {
+        registerWrite(0);
+    }
+
+    exit(0);
+}
 
 void settings(){
     gpio_config_t pin;
@@ -82,16 +102,4 @@ void sigintHandler(int sig_num)
     exit(1);
 }
 
-int main(int argc, char **argv){
-    signal(SIGINT, sigintHandler);
-    for (int c = 0; c < 256; c++) {
-        registerWrite(c);
-        usleep(50000);
-    }
-    usleep(50000);
-    for (int i = 0; i < 2; i++) {
-        registerWrite(0);
-    }
 
-    exit(0);
-}
